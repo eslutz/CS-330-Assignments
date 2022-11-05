f5e3f8e83321b6041fa71dd9a0407aaec1a526ba
Eric Slutz <eric.slutz@icloud.com>
2022-11-05 06:44:32 -0400

    introduced error in tutorial 2.5 shader to test error checking

--- module02/tut_02_05.cpp
+++ module02/tut_02_05.cpp
@@ -50,7 +50,7 @@ const char *vertexShaderSource = "#version 440 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, aPos.y, aPos.z, 1.0);\n" // Removed aPos.x to introduce error for testing
     "}\0";
 
 
