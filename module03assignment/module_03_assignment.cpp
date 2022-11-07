1590c7107848c39936febcc843d6b4d117f2447d
Eric Slutz <eric.slutz@icloud.com>
2022-11-07 06:45:19 -0500

    setup module 3 assignment

---
+++ module03assignment/module_03_assignment.cpp
@@ -0,0 +1,36 @@
+/*
+Module 3 Assignment
+Eric Slutz
+*/
+
+#include <iostream>         // cout, cerr
+#include <cstdlib>          // EXIT_FAILURE
+#include <GL/glew.h>        // GLEW library
+#include <GLFW/glfw3.h>     // GLFW library
+
+using namespace std; // Uses the standard namespace
+
+// Unnamed namespace
+namespace
+{
+    const char* const WINDOW_TITLE = "Module 3 Assignment"; // Macro for window title
+
+    // Variables for window width and height
+    const int WINDOW_WIDTH = 800;
+    const int WINDOW_HEIGHT = 600;
+
+    // Stores the GL data relative to a given mesh
+    struct GLMesh
+    {
+        GLuint vao;         // Handle for the vertex array object
+        GLuint vbos[2];     // Handles for the vertex buffer objects
+        GLuint nIndices;    // Number of indices of the mesh
+    };
+
+    // Main GLFW window
+    GLFWwindow* gWindow = nullptr;
+    // Triangle mesh data
+    GLMesh gMesh;
+    // Shader program
+    GLuint gProgramId;
+}
