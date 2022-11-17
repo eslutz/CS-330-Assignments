2419c4b79dc3971f7a32de32e0eefc37affd2265
Eric Slutz <eric.slutz@icloud.com>
2022-11-16 22:42:53 -0500

    modified tutorial 4.4 mouse sensitivity and cursor mode

--- module04tutorials/tut_04_04.cpp
+++ module04tutorials/tut_04_04.cpp
@@ -179,7 +179,7 @@ bool UInitialize(int argc, char* argv[], GLFWwindow** window)
     glfwSetMouseButtonCallback(*window, UMouseButtonCallback);
 
     // tell GLFW to capture our mouse
-    glfwSetInputMode(*window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(*window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
 
     // GLEW: initialize
     // ----------------
