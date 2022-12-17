8dc5ed2fa41e76b2f38a471a0fd8fda63eccc8df
Eric Slutz <eric.slutz@icloud.com>
2022-12-17 15:00:23 -0500

    set circle colors and upgrades

--- module08assignment/module_08_assignment.cpp
+++ module08assignment/module_08_assignment.cpp
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include<tuple>
+#include <tuple>
 #include <vector>
 #include <windows.h>
 
@@ -25,33 +25,31 @@ using namespace std;
 const float DEG2RAD = 3.14159 / 180;
 const int window_width = 480;
 
-void processInput(GLFWwindow* window);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
-enum BRICKTYPE { REFLECTIVE, DESTRUCTABLE };
-enum ONOFF { ON, OFF };
-enum DIRECTION { UP, RIGHT, DOWN, LEFT };
+enum BRICKTYPE { REFLECTIVE, DESTRUCTABLE }; // Represents a destructable or non-destructable brick
+enum ONOFF { ON, OFF }; // Represents the on or off state of a brick or circle
+enum DIRECTION { UP, RIGHT, DOWN, LEFT }; // Represents the four possible collision directions
+
+// Defines a Collision typedef that represents collision data
+typedef std::tuple<bool, DIRECTION, glm::vec2> Collision;
 
 class Brick
 {
 public:
-
-	float x, y, width;
-	float r, g, b;
+	glm::vec2 position;
+	float width;
 	BRICKTYPE brick_type;
 	ONOFF onoff;
 	int health;
 
-	Brick(BRICKTYPE bt, float xx, float yy, float ww)
+	Brick(BRICKTYPE bt, glm::vec2 pos, float ww)
 	{
 		brick_type = bt;
-		x = xx;
-		y = yy,
+		position = pos;
 		width = ww;
 		onoff = ON;
 		health = 50;
-		r = 0.0f;
-		g = 0.0f;
-		b = 1.0f;
 	};
 
 	void drawBrick()
@@ -67,26 +65,26 @@ public:
 			else
 			{
 				// Set brick color
-				float color = health % 10 == 0 ? 1 : (float)(0.4 + (1 - 0.4) * ((health % 10) - 0) / (9 - 0));
+				float color = health % 10 == 0 ? 1 : (float)(0.4 + (1 - 0.4) * ((health % 10) / 9.0));
 				if (health > 40)
 				{
-					glColor3d(0, 0, color);
+					glColor3d(0.0f, 0.0f, color);
 				}
 				else if (health > 30)
 				{
-					glColor3d(0, color, color);
+					glColor3d(0.0f, color, color);
 				}
 				else if (health > 20)
 				{
-					glColor3d(0, color, 0);
+					glColor3d(0.0f, color, 0.0f);
 				}
 				else if (health > 10)
 				{
-					glColor3d(color, color, 0);
+					glColor3d(color, color, 0.0f);
 				}
 				else if (health > 0)
 				{
-					glColor3d(color, 0, 0);
+					glColor3d(color, 0.0f, 0.0f);
 				}
 				else
 				{
@@ -96,10 +94,10 @@ public:
 
 			glBegin(GL_POLYGON);
 
-			glVertex2d(x + halfside, y + halfside);
-			glVertex2d(x + halfside, y - halfside);
-			glVertex2d(x - halfside, y - halfside);
-			glVertex2d(x - halfside, y + halfside);
+			glVertex2d(position.x + halfside, position.y + halfside);
+			glVertex2d(position.x + halfside, position.y - halfside);
+			glVertex2d(position.x - halfside, position.y - halfside);
+			glVertex2d(position.x - halfside, position.y + halfside);
 
 			glEnd();
 		}
@@ -113,24 +111,31 @@ public:
 	float radius;
 	glm::vec2 velocity;
 	glm::vec3 color;
+	ONOFF onoff;
+	int health;
+	int strength;
 
-	Circle(glm::vec2 pos, float rad, glm::vec2 vel, glm::vec3 col)
+	Circle(glm::vec2 pos, float rad, glm::vec2 vel)
 	{
 		position = pos;
 		radius = rad;
+		color = glm::vec3(1.0f, 0.0f, 0.0f);
 		velocity = vel;
-		color = col;
+		onoff = ON;
+		health = 100;
+		strength = 1;
 	}
 
+	// Changes movement of circle if there is a collsion
 	void CheckCollision(Brick* brk)
 	{
-		tuple<boolean, DIRECTION, glm::vec2> collision = Collision(brk);
+		Collision collision = CollisionHappened(brk);
 		// If collision is true
 		if (std::get<0>(collision))
 		{
 			if (brk->brick_type == DESTRUCTABLE)
 			{
-				brk->health--;
+				brk->health -= strength;
 			}
 			// Collision resolution
 			if (brk->onoff == ON)
@@ -168,33 +173,51 @@ public:
 		{
 			velocity.x = -velocity.x;
 			position.x = -0.99f;
+			std::get<0>(collision) = true;
 		}
 		else if (position.x >= 1)
 		{
 			velocity.x = -velocity.x;
 			position.x = 0.99f;
+			std::get<0>(collision) = true;
 		}
 		else if (position.y <= -1.0f)
 		{
 			velocity.y = -velocity.y;
 			position.y = -0.99f;
+			std::get<0>(collision) = true;
 		}
 		else if (position.y >= 1.0f)
 		{
 			velocity.y = -velocity.y;
 			position.y = 0.99f;
+			std::get<0>(collision) = true;
+		}
+
+		// If brick or wall collision occurs, decrease circle health
+		if (std::get<0>(collision))
+		{
+			if (health > 0)
+			{
+				health--;
+			}
+			else
+			{
+				onoff = OFF;
+			}
 		}
 	}
 
-	tuple<boolean, DIRECTION, glm::vec2> Collision(Brick* brk) // AABB - Circle collision
+	// Checks for circle collision with a brick
+	Collision CollisionHappened(Brick* brk) // AABB - Circle collision
 	{
 		// Get center point circle first 
 		glm::vec2 center(glm::vec2(position.x, position.y) + radius);
 		// Calculate AABB info (center, half-extents)
 		glm::vec2 aabb_half_extents(brk->width / 2.0f, brk->width / 2.0f);
 		glm::vec2 aabb_center(
-			brk->x + aabb_half_extents.x,
-			brk->y + aabb_half_extents.y
+			brk->position.x + aabb_half_extents.x,
+			brk->position.y + aabb_half_extents.y
 		);
 		// Get difference vector between both centers
 		glm::vec2 difference = center - aabb_center;
@@ -210,6 +233,7 @@ public:
 			return make_tuple(false, UP, glm::vec2(0.0f, 0.0f));
 	}
 
+	// Get new direction after collision
 	DIRECTION VectorDirection(glm::vec2 target)
 	{
 		// Get new direction
@@ -233,20 +257,85 @@ public:
 		return (DIRECTION)best_match;
 	}
 
+	// Check for circle collision with another circle
+	bool CheckCollision(Circle* cir)
+	{
+		// Collision x-axis?
+		bool collisionX = position.x + radius >= cir->position.x &&
+			cir->position.x + cir->radius >= position.x;
+		// Collision y-axis?
+		bool collisionY = position.y + radius >= cir->position.y &&
+			cir->position.y + cir->radius >= position.y;
+
+		// Collision only if on both axes
+		return collisionX && collisionY;
+	}
+
+	// Upgrades circle stats when it consumes another circle
+	void LevelUp(Circle* cir)
+	{
+		// Consuming circle gains remaining health of circle consumed
+		health += cir->health;
+		// Consuming circle gains strength of circle consumed
+		for (int i = 0; i < cir->strength; i++)
+		{
+			strength++;
+			// Every 5 strength level increases will increase the size of the circle
+			// Size maxes out at strength level 20
+			if ((strength <= 20) && (strength % 5 == 0))
+			{
+				radius += 0.015f;
+			}
+		}
+	}
+
 	void DrawCircle()
 	{
-		glColor3f(color.r, color.g, color.b);
-		glBegin(GL_POLYGON);
-		for (int i = 0; i < 360; i++)
+		if (onoff == ON)
 		{
-			float degInRad = i * DEG2RAD;
-			glVertex2f((cos(degInRad) * radius) + position.x, (sin(degInRad) * radius) + position.y);
+			// Set circle color
+			float color = health % 250 == 0 ? 1 : (float)(0.4 + (1 - 0.4) * ((health % 250) / 249.0));
+			if (health > 1500)
+			{
+				glColor3d(1.0f, 0.0f, 1.0f);
+			}
+			else if (health > 1250)
+			{
+				glColor3d(color, 0.0f, color);
+			}
+			else if (health > 1000)
+			{
+				glColor3d(0.0f, 0.0f, color);
+			}
+			else if (health > 750)
+			{
+				glColor3d(0.0f, color, color);
+			}
+			else if (health > 500)
+			{
+				glColor3d(0.0f, color, 0.0f);
+			}
+			else if (health > 250)
+			{
+				glColor3d(color, color, 0.0f);
+			}
+			else if (health > 0)
+			{
+				glColor3d(color, 0.0f, 0.0f);
+			}
+
+			glBegin(GL_POLYGON);
+			for (int i = 0; i < 360; i++)
+			{
+				float degInRad = i * DEG2RAD;
+				glVertex2f((cos(degInRad) * radius) + position.x, (sin(degInRad) * radius) + position.y);
+			}
+			glEnd();
 		}
-		glEnd();
 	}
 };
 
-vector<Circle> world;
+vector<Circle> circles;
 vector<Brick> bricks;
 
 // Create brick pattern
@@ -283,6 +372,8 @@ int main(void)
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
 
+	glfwSetKeyCallback(window, key_callback);
+
 	for (int i = 0; i < gameGrid.size(); i++)
 	{
 		for (int j = 0; j < gameGrid[i].size(); j++)
@@ -295,11 +386,11 @@ int main(void)
 				// 1 in 6 chance the brick will be indestructable
 				if (rand() % 6 == 0)
 				{
-					bricks.push_back(Brick(REFLECTIVE, xPos, yPos, 0.19));
+					bricks.push_back(Brick(REFLECTIVE, glm::vec2(xPos, yPos), 0.19));
 				}
 				else
 				{
-					bricks.push_back(Brick(DESTRUCTABLE, xPos, yPos, 0.19));
+					bricks.push_back(Brick(DESTRUCTABLE, glm::vec2(xPos, yPos), 0.19));
 				}
 			}
 		}
@@ -316,16 +407,31 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0.412f, 0.412f, 0.412f, 1.0f);
 
-		processInput(window);
-
-		// Movement
-		for (int i = 0; i < world.size(); i++)
+		// Move and draw circles
+		for (int i = 0; i < circles.size(); i++)
 		{
-			for (int x = 0; x < bricks.size(); x++)
+			if (circles[i].onoff == OFF)
 			{
-				world[i].CheckCollision(&bricks[x]);
+				circles.erase(circles.begin() + i);
 			}
-			world[i].DrawCircle();
+			else
+			{
+				for (int x = 0; x < bricks.size(); x++)
+				{
+					circles[i].CheckCollision(&bricks[x]);
+				}
+				for (int y = 0; y < circles.size(); y++)
+				{
+					if (i != y &&
+						circles[i].CheckCollision(&circles[y]) &&
+						circles[i].strength >= circles[y].strength)
+					{
+						circles[i].LevelUp(&circles[y]);
+						circles[y].onoff = OFF;
+					}
+				}
+				circles[i].DrawCircle();
+			}
 		}
 
 		// Draw bricks
@@ -350,18 +456,15 @@ int main(void)
 	exit(EXIT_SUCCESS);
 }
 
-void processInput(GLFWwindow* window)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
 	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
 	{
-		double r, g, b;
-		r = rand() / 10000;
-		g = rand() / 10000;
-		b = rand() / 10000;
-		Circle B(glm::vec2(0, -1), 0.05, glm::vec2(0.0005f, 0.0005f), glm::vec3(r, g, b));
-		world.push_back(B);
+		// velocity.x: neg goes left, pos goes right, 1 goes vertical, 9 goes horizontal
+		Circle B(glm::vec2(0.0f, -1.0f), 0.05, glm::vec2(-0.0005f, 0.0005f));
+		circles.push_back(B);
 	}
 }
